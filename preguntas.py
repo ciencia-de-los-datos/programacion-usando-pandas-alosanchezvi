8bf469ab9f529978c54476db992e63332cec266d
alosanchezvi <59671162+alosanchezvi@users.noreply.github.com>
2023-03-10 17:45:40 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -149,7 +149,7 @@ def pregunta_09():
     39   39   E    5  1998-01-26  1998
 
     """
-    tbl0['year']=tbl0.apply(lambda x: pd.to_datetime(x['_c3']).dt.year ,axis=1)
+    tbl0['year']=pd.to_datetime(tbl0['_c3']).dt.year
     return tbl0
 
 
